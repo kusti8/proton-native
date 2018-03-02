6c783b1d1d1677f3ea61858519afa19a7acb449d
Caleb Hattingh <caleb.hattingh@gmail.com>
2018-03-03 09:50:56 +1000

    Make python hello-world example comparable to proton example

--- docs/python_example.py
+++ docs/python_example.py
@@ -1,33 +1,15 @@
 import sys
-from PyQt5.QtWidgets import (QWidget, QToolTip, 
-    QPushButton, QApplication)
-from PyQt5.QtGui import QFont    
+from PyQt5 import QtWidgets
 
-
-class Example(QWidget):
-    
+class Example(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
-        
-        self.initUI()
-
-    def print_hello(self):
-        print("Hello")
-        
-        
-    def initUI(self):
-        btn = QPushButton('Button', self)
-        btn.clicked.connect(self.print_hello)
-        btn.resize(btn.sizeHint())
-        btn.move(50, 50)       
-        
+        btn = QtWidgets.QPushButton('Button', self)
+        btn.clicked.connect(lambda: print('hello'))
         self.setGeometry(300, 300, 300, 200)
-        self.setWindowTitle('Example')    
+        self.setWindowTitle('Example')
         self.show()
-        
-        
-if __name__ == '__main__':
-    
-    app = QApplication(sys.argv)
-    ex = Example()
-    sys.exit(app.exec_())
+
+app = QtWidgets.QApplication([])
+ex = Example()
+sys.exit(app.exec_())
