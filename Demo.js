c6af16c1991cf74024378b3d6086534f45e14921
kusti8 <kusti8@gmail.com>
2018-05-16 18:56:38 -0400

    Reset demo

--- Demo.js
+++ Demo.js
@@ -1,17 +1,13 @@
 import React, { Component } from 'react';
 
-import { render, Window, App, Slider, Box, Button } from './src/';
+import { render, Window, App, TextInput, Dialog } from './src/';
 
 class Example extends Component {
-  state = { min: 20 };
   render() {
     return (
       <App>
         <Window title="Test" size={{ w: 500, h: 500 }} margined={true}>
-          <Box>
-            <Slider stretchy={false} min={this.state.min} max={25} />
-            <Button onClick={() => this.setState({ min: 10 })} />
-          </Box>
+          <TextInput stretchy={false} />
         </Window>
       </App>
     );
