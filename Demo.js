b3edcd061349a846163e71d569b03e49f0fb7c24
Niklas Mischkulnig <mischnic@users.noreply.github.com>
2018-05-20 17:07:02 +0200

    Reset demo

--- Demo.js
+++ Demo.js
@@ -1,14 +1,11 @@
 import React, { Component } from 'react';
 
-import { render, Window, App, TextInput, Dialog, Menu } from './src/';
+import { render, Window, App, TextInput, Dialog } from './src/';
 
 class Example extends Component {
   render() {
     return (
       <App>
-        <Menu>
-          <Menu.Item type="Quit" />
-        </Menu>
         <Window title="Test" size={{ w: 500, h: 500 }} margined={true}>
           <TextInput stretchy={false} />
         </Window>
