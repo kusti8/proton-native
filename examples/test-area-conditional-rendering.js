ca789cbc00269d26e97025d9bd22c05b7e793175
Niklas Mischkulnig <mischnic@users.noreply.github.com>
2018-09-01 15:54:43 +0200

    Fix updating of AreaText/-Group children (#172)
    * Fix conditional rendering of children in Area
    
    * Fix updating of Area.Text
    
    Closes #171
    
    * Add Demo.js to prettier script
    
    * Fixup for AreaText

---
+++ examples/test-area-conditional-rendering.js
@@ -0,0 +1,54 @@
+// When proper testing is implemented, migrate this
+
+import React, { Component } from 'react';
+
+import { render, Window, Box, App, Text, Area, Button } from '../src/';
+
+class Example extends Component {
+  state = { flag: false };
+
+  render() {
+    const flagText = 'Flag is ' + (this.state.flag ? 'Set' : 'Not Set');
+
+    return (
+      <App>
+        <Window title="Test" size={{ w: 500, h: 500 }} margined={true}>
+          <Box padded>
+            <Button
+              onClick={() => this.setState({ flag: !this.state.flag })}
+              stretchy={false}
+            >
+              Toggle Flag
+            </Button>
+            <Text stretchy={false}>{flagText}</Text>
+            <Area>
+              <Area.Text>
+                {this.state.flag && (
+                  <Area.Text style={{ fontStyle: 'italic' }}>
+                    Italic{'\n'}
+                  </Area.Text>
+                )}
+                {flagText}
+                {'\n'}
+                {this.state.flag && (
+                  <Area.Text style={{ color: 'red' }}>Red</Area.Text>
+                )}
+              </Area.Text>
+              <Area.Group>
+                {this.state.flag && (
+                  <Area.Circle fill="red" x="100" y="100" r="20" />
+                )}
+                <Area.Circle fill="green" x="150" y="100" r="20" />
+                {this.state.flag && (
+                  <Area.Circle fill="blue" x="200" y="100" r="20" />
+                )}
+              </Area.Group>
+            </Area>
+          </Box>
+        </Window>
+      </App>
+    );
+  }
+}
+
+render(<Example />);
