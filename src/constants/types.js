48682e461aab29e48bda50a96998b9154beffa37
Gustav Hansen <kusti8@gmail.com>
2019-05-05 18:40:23 -0400

    Start on rewriting registerComponent

--- src/constants/types.js
+++
@@ -1,6 +0,0 @@
-export const ITEM = 'Item';
-export const CHECK = 'Check';
-export const QUIT = 'Quit';
-export const PREFERENCES = 'Preferences';
-export const ABOUT = 'About';
-export const SEPARATOR = 'Separator';
--- src/render/index.js
+++ src/render/index.js
@@ -1,22 +1,11 @@
-import { createElement } from '../utils/createElement';
-import DesktopRenderer from '../reconciler/';
 import { connectDevtools } from '../devtools';
+import DesktopRenderer from '../reconciler';
 
-export let ROOT_NODE = {};
-
-// Renders the input component
-function render(element) {
+function registerComponent(name, component) {
   connectDevtools(DesktopRenderer);
 
-  ROOT_NODE = createElement('ROOT');
-  const container = ROOT_NODE;
-
-  // Returns the current fiber (flushed fiber)
-  const node = DesktopRenderer.createContainer(ROOT_NODE);
-
-  // Schedules a top level update with current fiber and a priority level (depending upon the context)
-  DesktopRenderer.updateContainer(element, node, null);
-  //ROOT_NODE.render();
+  const container = DesktopRenderer.createContainer(window);
+  DesktopRenderer.updateContainer(element, container, null);
 }
 
 export default render;
