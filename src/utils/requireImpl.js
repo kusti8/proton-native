2f3ba4fcf64721c858f338207abe4060bafc37c1
kusti8 <kusti8@gmail.com>
2019-09-21 14:14:59 -0400

    Fix require not working with jest

--- src/utils/requireImpl.js
+++ src/utils/requireImpl.js
@@ -7,7 +7,12 @@ import sizeOf from 'image-size';
 const originalLoader = Module._load;
 
 Module._load = function(request, parent) {
-  const buffer = readChunk.sync(request, 0, fileType.minimumBytes);
+  let buffer;
+  try {
+    buffer = readChunk.sync(request, 0, fileType.minimumBytes);
+  } catch {
+    return originalLoader.apply(this, arguments);
+  }
   const type = fileType(buffer).mime;
   if (type.split('/')[0] != 'image')
     return originalLoader.apply(this, arguments);
